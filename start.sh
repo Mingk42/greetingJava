b2e32172210f0f501a38ed7a7ac38a79f1dcba2e
Mingk42 <xoals123456t@gmail.com>
2024-10-21 11:20:54 +0900

    std in

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 
 # Start the application
 # nohup java -jar build/libs/greeting-0.0.1-SNAPSHOT.jar &
-nohup java -jar ~/wd/app.jar &
+nohup java -jar ~/wd/app.jar > server.log 2>&1 &
 
 echo "> Application started with PID $!"
