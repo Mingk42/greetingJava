761ab9930e67df69e238bbd37f4a675f15991ea8
Mingk42 <xoals123456t@gmail.com>
2024-10-23 16:49:08 +0900

    add jpa dependency

--- src/main/java/shop/samdul/greeting/repository/TodoRepository.java
+++ src/main/java/shop/samdul/greeting/repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package shop.samdul.greeting.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import shop.samdul.greeting.entitiy.TodoEntity;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
